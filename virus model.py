8b117d3d4b74e2d388d08009117a359e5da81517
KiARC <katiearose@protonmail.com>
2022-11-01 12:21:44 -0400

    Contour and DotPlot together
    For submission to teacher

--- virus model.py
+++ virus model.py
@@ -81,6 +81,11 @@ gamma = 1 / 4
 beta_array = linspace(0.1, 1.1, 10)
 gamma_array = linspace(0.1, 0.7, 5)
 frame = sweep_parameters(beta_array, gamma_array)
+contour(frame)
+decorate(xlabel='Recovery rate (gamma)',
+         ylabel='Contact rate (beta)',
+         title='Contour plot, fraction infected')
+plt.show()
 plot_sweep_frame(frame)
 decorate(xlabel='Contact number (beta/gamma)',
          ylabel='Fraction infected')
