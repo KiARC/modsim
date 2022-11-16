8cff04ed45cfa5d63075d6649c937a85c28e4c21
KiARC <katiearose@protonmail.com>
2022-11-16 08:18:39 -0500

    Mix function

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ from modsim import TimeSeries, System
 
 coffee = System(t=90, r=0.01, v=350)
 milk = System(t=5, r=0.061086056, v=50)
+
+
+def mix(*systems):
+    a = 0
+    for system in systems:
+        a += system.v * system.t
+    total_volume = 0
+    for system in systems:
+        total_volume += system.v
+    temperature = a / total_volume
+    r = 0
+    for system in systems:
+        r += system.v * system.r
+    r /= total_volume
+    return System(temperature, total_volume, r)
